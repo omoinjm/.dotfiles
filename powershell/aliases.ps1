bfd17978193ac9bd0930dc52f1108693a19dc3fe
njmalaza <junior.malaza@rysis.co.za>
2022-11-20 21:42:08 +0200

    first commit

---
+++ powershell/aliases.ps1
@@ -0,0 +1,106 @@
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias idea idea64
+Set-Alias -Name eth -Value Get-NetAdapter
+Set-Alias -Name Pywal-Starship -Value pywal_to_starship
+Set-Alias -Name Wallpaper-Starship -Value update_pywal_to_starship
+Set-Alias -Name Screenshot-Starship -Value screenshot_to_pywal_to_starship
+Set-Alias -Name vim nvim
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Git commands
+function gpull { git pull origin master }
+function gpush { git push origin master }
+function gcheck { git checkout master }
+function gs { git status }
+function gres { git restore . }
+
+   ## See commits
+   function glg { git log }
+  
+   ## all commits on all branches that have not been pushed 
+   function glog {
+	git log --branches --not --remotes
+   }
+
+   ## most recent commit on each branch
+   function gloge {
+   	git log --branches --not --remotes --simplify-by-decoration --decorate --oneline
+   }
+
+## add, commit and push
+function acp($message) {
+    git add .
+    git commit -m "$message"
+    git push origin HEAD
+}
+
+## delete local branch
+function gdelete($branch) {
+	git branch -D $branch
+}
+
+# Open VSC and Serve
+function cng { code . && ng serve -o }
+
+# Work repos
+
+### CHRONODESK
+function chrono { cd "D:\Rysis\Git\rysis-chronolog\" }
+function chronoApi { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_API\" }
+function chronoWeb { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Web\ClientApp\" }
+function chronoSql { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Midtier\SqlProxyClassGen\" }
+
+### VISIBILL
+function visibill { cd "D:\Rysis\Git\visibill-4\" }
+function visibillApi { cd "D:\Rysis\Git\visibill-4\Visibill_API" }
+function visibillWeb { cd "D:\Rysis\Git\visibill-4\VisibillWeb\ClientApp" }
+function visibillSql { cd "D:\Rysis\Git\visibill-4\Visibill_Midtier\SqlProxyClassGen\" }
+
+### VELOCITY
+function velo { cd "D:\Rysis\Git\rysis-driverisk2\" }
+function veloApi { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Api" }
+function veloWeb { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Drops\ClientApp\" }
+function veloSql { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Midtier\SqlProxyClassGen\" }
+
+### DRIVERISK
+function drive { cd "D:\Rysis\Git\rysis-driverisk\" }
+function drops { cd "D:\Rysis\Git\rysis-driverisk\DriveRisk.Drops.WebApp\" }
+
+### SAHCS
+function sahiv { cd "D:\Rysis\GIT\rysis-sahiv\Website\" }
+function sahivApi { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Api" }
+function sahivSql { cd "D:\Rysis\GIT\rysis-sahiv\Website\Sahiv.Midtier2\Bat2\" }
+function sahivWeb { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Angular\ClientApp" }
+
+### ATRAX
+function atrax { cd "D:\Rysis\Git\rysis-atrax2\" }
+function atraxApi { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Api" }
+function atraxWeb { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Webapp\ClientApp\" }
+function atraxSql { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Midtier\SqlProxyClassGen\" }
+
+### EXEC MOBILE
+function exec { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile" }
+function execApi { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Api" }
+function execWeb { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Web\ClientApp" }
+function execSql { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Sql\SqlScripts" }
+
+### PRESS COUNCIL
+function press { cd "D:\Rysis\Git\rysis-pressombudsman\" }
+function pressSql { cd "D:\Rysis\Git\rysis-pressombudsman\PressOmbudsman.Midtier\Bat2\" }
+
+### PERSONAL DIR
+function omoiDev { cd "C:\Users\nhlan\Desktop\Development" }
+function omoiGit { cd "C:\Users\nhlan\Desktop\Development\Git" }
+function omoiSite { cd "C:\Users\nhlan\Desktop\Development\Git\portfolio" }
+
+### STARSHIP THEME
+function star { Invoke-Expression (&starship init powershell) }
---
+++ powershell/user_profile.ps1
@@ -0,0 +1,51 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# IMPORTS
+
+## Prompt
+Import-Module posh-git
+oh-my-posh init pwsh | Invoke-Expression
+## Set-PoshPrompt Paradox
+
+## Icons
+Import-Module Terminal-Icons
+
+# Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'themes/json.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionViewStyle ListView 
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsfzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+$env:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+
+# UPDATE SCRIPTS
+
+function pywal_to_starship {
+	Update-WalTerminal *>&1;
+	bash ~/dev/winwaltostarship.sh;
+	bash ~/dev/waltostarship.sh;
+}
+
+function update_pywal_to_starship{
+	Update-WalTheme *>&1;
+	bash ~/dev/winwaltostarship.sh;
+	bash ~/dev/waltostarship.sh;
+}
+
+function screenshot_to_pywal_to_starship{
+	Get-Wallpaper;
+	pywal_to_starship;
+}
