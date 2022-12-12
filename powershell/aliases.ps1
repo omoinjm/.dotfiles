c2aea52d9e3c0be17e03cba3dbe5d86950ecccf1
njmalaza <junior.malaza@rysis.co.za>
2022-12-12 12:20:48 +0200

    dirs and custom shell

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,4 +1,5 @@
 # Alias
+. $HOME\.config\powershell\dirs.ps1
 Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias g git
@@ -9,6 +10,7 @@ Set-Alias -Name Pywal-Starship -Value pywal_to_starship
 Set-Alias -Name Wallpaper-Starship -Value update_pywal_to_starship
 Set-Alias -Name Screenshot-Starship -Value screenshot_to_pywal_to_starship
 Set-Alias -Name vim nvim
+Set-Alias -Name curl -Value "C:\Program Files\Git\mingw64\bin\curl.exe" -Option AllScope
 
 # Utilities
 
@@ -61,56 +63,6 @@ function gdelete($branch) {
 # Open VSC and Serve
 function cng { code . && ng serve -o }
 
-# Work repos
-
-### CHRONODESK
-function chrono { cd "D:\Rysis\Git\rysis-chronolog\" }
-function chronoApi { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_API\" }
-function chronoWeb { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Web\ClientApp\" }
-function chronoSql { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Midtier\SqlProxyClassGen\" }
-
-### VISIBILL
-function visibill { cd "D:\Rysis\Git\visibill-4\" }
-function visibillApi { cd "D:\Rysis\Git\visibill-4\Visibill_API" }
-function visibillWeb { cd "D:\Rysis\Git\visibill-4\VisibillWeb\ClientApp" }
-function visibillSql { cd "D:\Rysis\Git\visibill-4\Visibill_Midtier\SqlProxyClassGen\" }
-
-### VELOCITY
-function velo { cd "D:\Rysis\Git\rysis-driverisk2\" }
-function veloApi { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Api" }
-function veloWeb { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Drops\ClientApp\" }
-function veloSql { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Midtier\SqlProxyClassGen\" }
-
-### DRIVERISK
-function drive { cd "D:\Rysis\Git\rysis-driverisk\" }
-function drops { cd "D:\Rysis\Git\rysis-driverisk\DriveRisk.Drops.WebApp\" }
-
-### SAHCS
-function sahiv { cd "D:\Rysis\GIT\rysis-sahiv\Website\" }
-function sahivApi { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Api" }
-function sahivSql { cd "D:\Rysis\GIT\rysis-sahiv\Website\Sahiv.Midtier2\Bat2\" }
-function sahivWeb { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Angular\ClientApp" }
-
-### ATRAX
-function atrax { cd "D:\Rysis\Git\rysis-atrax2\" }
-function atraxApi { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Api" }
-function atraxWeb { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Webapp\ClientApp\" }
-function atraxSql { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Midtier\SqlProxyClassGen\" }
-
-### EXEC MOBILE
-function exec { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile" }
-function execApi { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Api" }
-function execWeb { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Web\ClientApp" }
-function execSql { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Sql\SqlScripts" }
-
-### PRESS COUNCIL
-function press { cd "D:\Rysis\Git\rysis-pressombudsman\" }
-function pressSql { cd "D:\Rysis\Git\rysis-pressombudsman\PressOmbudsman.Midtier\Bat2\" }
-
-### PERSONAL DIR
-function omoiDev { cd "C:\Users\nhlan\Desktop\Development" }
-function omoiGit { cd "C:\Users\nhlan\Desktop\Development\Git" }
-function omoiSite { cd "C:\Users\nhlan\Desktop\Development\Git\portfolio" }
 
 ### STARSHIP THEME
 function star { Invoke-Expression (&starship init powershell) }
---
+++ powershell/dirs.ps1
@@ -0,0 +1,50 @@
+# Work repos
+
+### CHRONODESK
+function chrono { cd "D:\Rysis\Git\rysis-chronolog\" }
+function chronoApi { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_API\" }
+function chronoWeb { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Web\ClientApp\" }
+function chronoSql { cd "D:\Rysis\Git\rysis-chronolog\Timesheet_Midtier\SqlProxyClassGen\" }
+
+### VISIBILL
+function visibill { cd "D:\Rysis\Git\visibill-4\" }
+function visibillApi { cd "D:\Rysis\Git\visibill-4\Visibill_API" }
+function visibillWeb { cd "D:\Rysis\Git\visibill-4\VisibillWeb\ClientApp" }
+function visibillSql { cd "D:\Rysis\Git\visibill-4\Visibill_Midtier\SqlProxyClassGen\" }
+
+### VELOCITY
+function velo { cd "D:\Rysis\Git\rysis-driverisk2\" }
+function veloApi { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Api" }
+function veloWeb { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Drops\ClientApp\" }
+function veloSql { cd "D:\Rysis\Git\rysis-driverisk2\DriveRisk.Midtier\SqlProxyClassGen\" }
+
+### DRIVERISK
+function drive { cd "D:\Rysis\Git\rysis-driverisk\" }
+function drops { cd "D:\Rysis\Git\rysis-driverisk\DriveRisk.Drops.WebApp\" }
+
+### SAHCS
+function sahiv { cd "D:\Rysis\GIT\rysis-sahiv\Website\" }
+function sahivApi { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Api" }
+function sahivSql { cd "D:\Rysis\GIT\rysis-sahiv\Website\Sahiv.Midtier2\Bat2\" }
+function sahivWeb { cd "D:\Rysis\Git\rysis-sahiv\Website\SahivV3_Angular\ClientApp" }
+
+### ATRAX
+function atrax { cd "D:\Rysis\Git\rysis-atrax2\" }
+function atraxApi { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Api" }
+function atraxWeb { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Webapp\ClientApp\" }
+function atraxSql { cd "D:\Rysis\Git\rysis-atrax2\Operational System\Atrax.Midtier\SqlProxyClassGen\" }
+
+### EXEC MOBILE
+function exec { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile" }
+function execApi { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Api" }
+function execWeb { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Web\ClientApp" }
+function execSql { cd "D:\Rysis\Git\rysis-execmobile\ExecMobile\ExecMobile.Sql\SqlScripts" }
+
+### PRESS COUNCIL
+function press { cd "D:\Rysis\Git\rysis-pressombudsman\" }
+function pressSql { cd "D:\Rysis\Git\rysis-pressombudsman\PressOmbudsman.Midtier\Bat2\" }
+
+### PERSONAL DIR
+function omoiDev { cd "C:\Users\nhlan\Desktop\Development" }
+function omoiGit { cd "C:\Users\nhlan\Desktop\Development\Git" }
+function omoiSite { cd "C:\Users\nhlan\Desktop\Development\Git\portfolio" }
