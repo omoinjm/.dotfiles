a2654a47440a33d1ed1cff151040700d4c1849be
omoinjm <njmcloud@gmail.com>
2023-09-24 15:28:11 +0200

    update

---
+++ powershell/helpers/scripts/functions/bash.ps1
@@ -0,0 +1,11 @@
+# This file converts bash scripts into executable windows functions/programs
+
+#Set-Variable -Name pass -Value "/mnt/d/msys64/usr/bin/pass"
+
+#function pass() {
+#	bash $pass
+#}
+
+#function pass_show() {
+#	bash -c "$pass show github/personal"
+#}
