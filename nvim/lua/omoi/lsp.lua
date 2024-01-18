f95ba6688518628dfa7a73b305fe5e8a6042fa1d
omoinjm <njmcloud@gmail.com>
2024-01-18 08:50:40 +0200

    debugger is going just need to adjust

--- nvim/lua/omoi/lsp.lua
+++ nvim/lua/omoi/lsp.lua
@@ -4,4 +4,12 @@ function M.toggleInlayHints()
   vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
 end
 
+M.on_attach = function(client, bufnr)
+  if not client.supports_method("textDocument/semanticTokens") then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
 return M
