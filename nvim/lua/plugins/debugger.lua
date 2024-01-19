8da98bf999ce8a17bd581207095efd92e3fe66d1
omoinjm <njmcloud@gmail.com>
2024-01-19 08:53:56 +0200

    theme change and lsp server for C#

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -3,15 +3,27 @@ return {
     "mfussenegger/nvim-dap",
 
     dependencies = {
+      {
+        "mfussenegger/nvim-dap-python",
+        -- stylua: ignore
+        keys = {
+          { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+          { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+        },
+        config = function()
+          local path = require("mason-registry").get_package("debugpy"):get_install_path()
+          require("dap-python").setup(path .. "/venv/bin/python")
+        end,
+      },
 
       -- fancy UI for the debugger
       {
         "rcarriga/nvim-dap-ui",
       -- stylua: ignore
-      keys = {
-        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-      },
+        keys = {
+          { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+        },
         opts = {},
         config = function(_, opts)
           -- setup dap config by VsCode launch.json file
