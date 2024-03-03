7cd14a9f740f56bd8e251d29bea06b89a27e3b80
omoinjm <njmcloud@gmail.com>
2024-03-03 21:29:10 +0200

    let's go

--- powershell/helpers/aliases.ps1
+++ powershell/helpers/aliases.ps1
@@ -6,7 +6,7 @@ Set-Alias grep findstr
 Set-Alias -Name vim nvim
 Set-Alias -Name eth -Value Get-NetAdapter
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-Set-Alias -Name curl -Value "C:\Program Files\Git\mingw64\bin\curl.exe" -Option AllScope
+# Set-Alias -Name curl -Value "C:\Program Files\Git\mingw64\bin\curl.exe" -Option AllScope
 Set-Alias -Name iis -Value "C:\Program Files\IIS Express\iisexpress.exe" -Option AllScope
 Set-Alias -Name openvpn -Value "C:\Program Files\OpenVPN\bin\openvpn.exe" -Option AllScope
 Set-Alias -Name cl -Value "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\bin\Hostx64\x64\cl.exe"
