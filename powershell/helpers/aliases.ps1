b8529d7fcb3f43379b5da163303dc00756dd0715
omoinjm <njmcloud@gmail.com>
2024-05-15 22:12:01 +0200

    there we go

--- powershell/helpers/aliases.ps1
+++ powershell/helpers/aliases.ps1
@@ -4,13 +4,14 @@ Set-Alias g git
 Set-Alias -Name idea -Value "C:\Program Files\JetBrains\IntelliJ IDEA 2023.2.1\bin\idea64.exe" 
 Set-Alias grep findstr
 Set-Alias -Name vim nvim
+Set-Alias -Name vi nvim
 Set-Alias -Name eth -Value Get-NetAdapter
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 # Set-Alias -Name curl -Value "C:\Program Files\Git\mingw64\bin\curl.exe" -Option AllScope
 Set-Alias -Name iis -Value "C:\Program Files\IIS Express\iisexpress.exe" -Option AllScope
 Set-Alias -Name openvpn -Value "C:\Program Files\OpenVPN\bin\openvpn.exe" -Option AllScope
 Set-Alias -Name cl -Value "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.36.32532\bin\Hostx64\x64\cl.exe"
-Set-Alias -Name touch -Value "C:\Program Files\Git\usr\bin\touch.exe" -Option AllScope
+# Set-Alias -Name touch -Value "C:\Program Files\Git\usr\bin\touch.exe" -Option AllScope
 
 # Utilities
 
