e7b4fb55f00d55f942dc637713898ac8245df217
omoinjm <njmcloud@gmail.com>
2025-01-12 22:27:55 +0200

    update fish config

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -54,8 +54,6 @@ if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
 
-# oh-my-posh init fish | source
-
-# With Theme
+# Theme
 oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubblesextra.omp.json' | source
 
