0ec44bdebf800238808778946c10b20d4261c9e4
omoinjm <njmcloud@gmail.com>
2025-12-16 13:48:03 +0200

    sweet one

--- src/config/fish/conf.d/theme.fish
+++ src/config/fish/conf.d/theme.fish
@@ -1,2 +1,2 @@
 oh-my-posh init fish --config \
-    'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/tonybaloney.omp.json' | source
+    'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jblab_2021.omp.json' | source
