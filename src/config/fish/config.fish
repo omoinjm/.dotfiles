521585d3568b5c0fd2c5d9b25ed9d4fac3a9ccc3
omoinjm <njmcloud@gmail.com>
2026-01-20 10:01:00 +0200

    I figured it out

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -27,6 +27,11 @@ set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 
+# Terminal theme
+oh-my-posh init fish --config \
+    'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubblesextra.omp.json' \
+    | source
+
 switch (uname)
     case Darwin
         source (dirname (status --current-filename))/config-osx.fish
