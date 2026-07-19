4e0dc9701dcf95ec32ff1b04d3de736d12ca6d61
omoinjm <njmcloud@gmail.com>
2026-07-19 04:20:41 +0200

    we good!

---
+++ home/.config/fish/conf.d/00-colors.fish
@@ -0,0 +1,11 @@
+# Fish UI colors tuned for dark terminals where brightBlack matches the background
+# (e.g. Solarized Osaka: background and brightBlack are both #001419).
+
+set -g fish_color_autosuggestion 555
+set -g fish_pager_color_completion brwhite
+set -g fish_pager_color_description yellow -i
+set -g fish_pager_color_prefix brcyan --bold --underline
+set -g fish_pager_color_progress brwhite --background=blue
+set -g fish_pager_color_selected_background --background=blue
+set -g fish_color_search_match bryellow --background=blue
+set -g fish_color_selection white --bold --background=blue
